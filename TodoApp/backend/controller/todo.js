f3e91b414dcb70921f1568816ff40da61168a25a
Yashaswini J <yashaswinij1999>
2024-06-16 13:02:26 +0530

    post request to add new todo

--- TodoApp/backend/controller/todo.js
+++ TodoApp/backend/controller/todo.js
@@ -6,3 +6,12 @@ exports.getData = async function (req, res) {
   const todos = await Todo.find({});
   res.send(todos);
 };
+
+exports.createTodo = async function (req, res) {
+  try {
+    const newTodo = await Todo.create(req.body);
+    res.status(201).send("todo added successfully");
+  } catch (error) {
+    res.status(500).send("server error");
+  }
+};
